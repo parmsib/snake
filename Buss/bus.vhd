1e2b288189cc3ca1999f27d795ee50d3d913ed20
sebbe <parsib01@hotmail.com>
2014-04-07 23:11:09 +0200

    added high impedance to bus

--- Buss/bus.vhd
+++ Buss/bus.vhd
@@ -7,7 +7,7 @@ entity mybus is
 	Port ( 	tobus, frombus : in STD_LOGIC_VECTOR(3 downto 0));
 end mybus;
 
-architecture Behavioral of mybus is
+architecture Behavioral1test of mybus is
 	signal realbus : STD_LOGIC_VECTOR(15 downto 0);
 	component instr_reg
 		Port (outgoing : in STD_LOGIC;
@@ -26,3 +26,53 @@ begin
 		end case;
 	end process;
 end Behavioral;
+
+
+architecture Behavioral2test of mybus is
+	signal realbus : STD_LOGIC_VECTOR(15 downto 0);
+	component instr_reg
+		Port (to_bus : in STD_LOGIC_VECTOR(15 downto 0);
+			  from_bus : out STD_LOGIC_VECTOR(15 downto 0);
+	end component;
+begin
+	process(tobus, frombus) begin
+		case tobus is
+			when "0001" => ir : instr_reg port map (X"ZZ", realbus);
+			...
+		end case;
+		case frombus is
+			when "0001" => ir : instr_reg port map (realbus, X"ZZ");
+			...
+		end case;
+	end process;
+end Behavioral;
+
+
+entity instr_reg is
+	Port( 
+		to_bus : in STD_LOGIC_VECTOR(15 downto 0);
+		from_bus : out STD_LOGIC_VECTOR(15 downto 0);)
+end instr_reg;
+
+architecture instrBeh of instr_ref is
+	signal instr : STD_LOGIC_VECTOR(15 downto 0)
+begin
+	process(clk) begin
+		if rising_edge(clk) then
+			if rst='1'
+				instr = X"00";
+			else:
+				instr <= from_bus;
+			end if;
+		end if;
+	end process;
+	to_bus <= instr;
+end instrBeh;
+
+
+
+
+
+
+
+		
