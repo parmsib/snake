8d7f3b7a5c8c04827b8a1058ed9bfbbd9d3deb26
Tobias Hultqvist <tobhu543@mux2-10.edu.isy.liu.se>
2014-05-16 01:53:07 +0200

    Jobbar med ALU... nytt hatobjekt

--- ALU_tb.vhd
+++ ALU_tb.vhd
@@ -1,126 +1,74 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+use ieee.std_logic_unsigned.all;
 
-ENTITY SPI_tb IS
-END SPI_tb;
+ENTITY ALU_tb IS
+END ALU_tb;
 
-architecture behv of SPI_tb is
-	component spi is
-		port ( 	clk : in std_logic;
-			buss : inout std_logic_vector(3 downto 0);
-			flags : inout std_logic_vector(6 downto 0);
-			miso : in std_logic;
-			sclk : out std_logic;
-			mosi : out std_logic;
-			ss : out std_logic
+architecture behv of ALU_tb is
+	component alu port(	
+		clk : in std_logic;
+		frombuss : in std_logic_vector(15 downto 0);
+		tobuss : out std_logic_vector(15 downto 0);
+		ALU : in std_logic_vector(3 downto 0);
+		tobus : in std_logic_vector(3 downto 0);
+		flags : out std_logic_vector(6 downto 0)
 		);
-                              
 	end component;
+	SIGNAL clk : std_logic := '0';
+	SIGNAL rst : std_logic := '0';
+	signal tb_running : boolean := true;
 
---	component GMEM is
---		port ( 	clk, rst : in STD_LOGIC;
---		--tile type
---		dbus_in : in STD_LOGIC_VECTOR ( 15 downto 0 ); 
---		--borde aldrig vara något annat än Z. ta bort?
---		dbus_out: out STD_LOGIC_VECTOR (15 downto 0); 
---		-- fr mikrokontroller
---		should_read_dbus, should_write_dbus : in STD_LOGIC; 
---		--fr Gadr 
---		write_adr: in STD_LOGIC_VECTOR (9 downto 0); 
---		-- fr SPI
---		read_adr: in STD_LOGIC_VECTOR (9 downto 0);  
---		-- till SPI
---		tile_type_out: out STD_LOGIC_VECTOR (3 downto 0)); 
---	end component;
-	
-  SIGNAL clk : std_logic := '0';
-  SIGNAL rst : std_logic := '0';
+	signal dbus : STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000"; 
+	signal dbus2 : STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";  
+	signal dflags : std_logic_vector(6 downto 0) := "0000000";
+	signal tobus : std_logic_vector(3 downto 0) := "0000";
 
-	signal dbus : STD_LOGIC_VECTOR(15 downto 0);  
-
-  signal tb_running : boolean := true;
-
---        signal seg : std_logic_vector(7 downto 0);
---        signal an : std_logic_vector(3 downto 0);
-
-		signal dflags : std_logic_vector(6 downto 0) := "0000000";
-		signal frombus : std_logic_vector(3 downto 0);
-		
-		signal miso : std_logic := '0';
-		signal sclk : std_logic := '0';
-		signal mosi : std_logic := '0';
-        
-		signal ss : std_logic := '0';
-
-		signal joystick_data : STD_LOGIC_VECTOR(0 to 79) := B"10101010_11001100_11100011_11110000_11111000_00011111_11101110_01000100_11111111_10000001";
+	signal dalu : std_logic_vector(3 downto 0) := "0100";
+	type arr is array(0 to 3) of std_logic_vector(15 downto 0);
+	signal buss_tmp : arr := (0 => X"0FF0",
+				  1 => X"03FF",
+				  2 => X"0FF0",
+				  3 => X"FF02");
+	signal dbus_tmp : std_logic_vector(15 downto 0) := "0000000000000000"; 
+	signal dalu_tmp : std_logic_vector(3 downto 0) := "0100";
 begin
-  --tile_type <= "0000";
-	uut: SPI port map (clk => clk,
-						buss => dbus(3 downto 0),
-						flags => dflags,
-						miso => miso,
-						sclk => sclk,
-						mosi => mosi);
-				--gmem_adr => tile_adr);
---	testmem: GMEM port map ( 	clk, rst, 
---					B"0000_0000_0000_0000",
---					dbus,
---					'0',
---					'0',
---					B"00000_00000",
---					tile_adr,
---					tile_type);
-					
-					
-							
+	uut: alu port map(	
+		clk => clk,
+		frombuss => dbus,
+		tobuss => dbus2,
+		ALU => dalu,
+		tobus => tobus,
+		flags => dflags
+		);					
+	dbus <= dbus_tmp;					
 	-- 100 MHz system clock
 	clk_gen : process
+	variable a : integer := -1;
 	begin
 	while tb_running loop
 		clk <= '0';
+			if a >= 0 and a <= 3 then
+				dbus_tmp <= buss_tmp(a);
+			end if;
+			if a = 4 then
+				dbus_tmp <= "0000000000000000"; 
+				dalu <= "0001";
+				dalu_tmp <= dalu_tmp + 1;
+			end if;
+			if a = 5 then
+				a := -1;
+				dalu <= dalu_tmp;
+			end if;
+			a := a + 1;
 		wait for 5 ns;
 		clk <= '1';
 		wait for 5 ns;
 	end loop;
 	wait;
 	end process;
-
-
-	miso_generator : process(sclk)
-	variable i : integer := 0;
-	begin
-		if rising_edge(sclk) then
-			miso <= joystick_data(i);
-			if i = 79 then
-				i := 0;
-			else
-				i := i + 1;
-			end if;
-		end if;
-	end process;
 			
-			
-
---	miso_generator : process
---	variable i : integer;
---	variable j : integer;
---	begin
---		i := 0;
---		j := 0;
---		while tb_running loop
---			wait for 1280 * 24 ns;
---			for i in (0 to 4) loop
---				miso <= joystick_data(i);
---				if i = 39 then
---					i := 0;
---				else
---					i := i + 1;
---				end if;
---				wait for 1280 ns;
---			end loop;
---		end loop;
---	end process;
   
 	stimuli_generator : process
 	variable i : integer;
@@ -128,13 +76,10 @@ begin
 		-- Aktivera reset ett litet tag.
 		rst <= '1';
 		wait for 500 ns;
-
 		wait until rising_edge(clk);        -- se till att reset släpps synkront
-										-- med klockan
+						    -- med klockan
 		rst <= '0';
-		report "Reset released" severity note;
-
-		
+		report "Reset released" severity note;		
 		wait;
 	end process;
 
