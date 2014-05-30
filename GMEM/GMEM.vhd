90ef8c970fd2ca6869668037be142af4b5e68c8a
Tobias Hultqvist <tobhu543@mux2-10.edu.isy.liu.se>
2014-05-30 16:00:02 +0200

    Simulering seems bra

--- GMEM/GMEM.vhd
+++ GMEM/GMEM.vhd
@@ -9,11 +9,9 @@ entity GMEM is
 	Port (
 		clk, rst : in STD_LOGIC;
 		--tile type
-		dbus_in : in STD_LOGIC_VECTOR ( 15 downto 0 ); 
-		--borde aldrig vara något annat än Z. ta bort?
-		dbus_out: out STD_LOGIC_VECTOR (15 downto 0); 
+		dbus : in STD_LOGIC_VECTOR ( 15 downto 0 ); 
 		-- fr mikrokontroller
-		should_read_dbus, should_write_dbus : in STD_LOGIC; 
+		frombus : in STD_LOGIC_VECTOR(3 downto 0);
 		--fr Gadr 
 		write_adr: in STD_LOGIC_VECTOR (9 downto 0); 
 		-- fr GPU
@@ -52,14 +50,14 @@ begin
 						mem(row, column) <= "0000";
 					end loop;
 				end loop;
-			elsif should_read_dbus = '1' then
+			elsif frombus = "1001" then
 				--write. address from write_adr. data from dbus
 				mem(	conv_integer(write_adr(9 downto 5)),
-					conv_integer(write_adr(4 downto 0))) <= dbus_in(15 downto 12);
+					conv_integer(write_adr(4 downto 0))) <= dbus(15 downto 12);
 			end if;
 		end if;
 	end process;
-	dbus_out <= "ZZZZZZZZZZZZZZZZ"; --känns lite dumt
+--	dbus_out <= "ZZZZZZZZZZZZZZZZ"; --känns lite dumt
 	tile_type_out <= mem(	conv_integer(read_adr(9 downto 5)),
 				conv_integer(read_adr(4 downto 0)));
 	--GPU: gpu port map (clk, rst, mem(read_adr(9 downto 5), read_adr(4 downto 0)), read_adr);
